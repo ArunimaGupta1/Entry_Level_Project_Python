0feaa92a4c967d9fc0e6ecd4651f30c31efbbe05
ArunimaGupta1 <arunimagupta71@gmail.com>
2021-03-16 20:58:35 +0530

    Mad libs game
    Ask the user for an input.
    
    This could be anything such as a name, an adjective, a pronoun or even an action. Once you get the input, you can rearrange it to build up your own story.

---
+++ Madlib.py
@@ -0,0 +1,15 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> adj = input("Enter an adjective : ")
+Enter an adjective : pretty
+>>> verb1 = input("Enter a verb :")
+Enter a verb :play
+>>> verb2 = input("Enter another verb :")
+Enter another verb :sleep
+>>> famous_person = input("Enter a famous person :")
+Enter a famous person :Amitabh Bachchan
+>>> madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}.\
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+>>> print(madlib)
+Computer programming is so pretty! It makes me so excited all the time because I love to play.I love to play. Stay hydrated and sleep like you are Amitabh Bachchan!
+>>> 
